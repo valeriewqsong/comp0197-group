6c9b41ece8760fdfe15846a31a66f2cd0631a6ea
meloods <melody.leom00@gmail.com>
2023-03-17 17:06:01 +0000

    lossfn.py added

---
+++ lossfn.py
@@ -0,0 +1,59 @@
+import torch
+import torch.nn.functional as F
+
+def semi_supervised_dice_loss(pred, target, pred_unlabeled, alpha, smooth=1):
+    """
+    Computes the semi-supervised Dice loss between labeled and unlabeled predictions and targets.
+    
+    Args:
+        pred (torch.Tensor): Labeled data predictions, shape (N, C, H, W)
+        target (torch.Tensor): Labeled data targets, shape (N, C, H, W)
+        pred_unlabeled (torch.Tensor): Unlabeled data predictions, shape (N, C, H, W)
+        alpha (float): The balancing factor between the labeled and unlabeled data contributions
+        smooth (float, optional): Smoothing factor to avoid division by zero, default=1
+
+    Returns:
+        torch.Tensor: The combined Dice loss for labeled and unlabeled data
+    """
+    # Labeled data
+    intersection_labeled = (pred * target).sum(dim=(2, 3))
+    union_labeled = pred.sum(dim=(2, 3)) + target.sum(dim=(2, 3))
+    dice_labeled = (2 * intersection_labeled + smooth) / (union_labeled + smooth)
+    dice_loss_labeled = 1 - dice_labeled.mean()
+
+    # Unlabeled data
+    true_label_unlabeled = (pred_unlabeled > 0.5).float()
+    intersection_unlabeled = (pred_unlabeled * true_label_unlabeled).sum(dim=(2, 3))
+    union_unlabeled = pred_unlabeled.sum(dim=(2, 3)) + true_label_unlabeled.sum(dim=(2, 3))
+    dice_unlabeled = (2 * intersection_unlabeled + smooth) / (union_unlabeled + smooth)
+    dice_loss_unlabeled = 1 - dice_unlabeled.mean()
+
+    # Combining the losses
+    loss = dice_loss_labeled + alpha * dice_loss_unlabeled
+
+    return loss
+
+def semi_supervised_iou_loss(pred, target, pred_unlabeled, alpha, eps=1e-6):
+    """
+    Computes the semi-supervised IoU loss between labeled and unlabeled predictions and targets.
+    
+    Args:
+        pred (torch.Tensor): Labeled data predictions, shape (N, C, H, W)
+        target (torch.Tensor): Labeled data targets, shape (N, C, H, W)
+        pred_unlabeled (torch.Tensor): Unlabeled data predictions, shape (N, C, H, W)
+        alpha (float): The balancing factor between the labeled and unlabeled data contributions
+        eps (float, optional): Small constant to avoid division by zero, default=1e-6
+
+    Returns:
+        torch.Tensor: The combined IoU loss for labeled and unlabeled data
+    """
+    # Labeled data
+    intersection_labeled = (pred * target).sum(dim=(2, 3))
+    union_labeled = pred.sum(dim=(2, 3)) + target.sum(dim=(2, 3)) - intersection_labeled
+    iou_labeled = (intersection_labeled + eps) / (union_labeled + eps)
+    iou_loss_labeled = 1 - iou_labeled.mean()
+
+    # Unlabeled data
+    true_label_unlabeled = (pred_unlabeled > 0.5).float()
+    intersection_unlabeled = (pred_unlabeled * true_label_unlabeled).sum(dim=(2, 3))
+    union_unlabeled = pred
