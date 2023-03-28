6637100a05b6f56b3faab40b4ef0c845468254b0
meloods <melody.leom00@gmail.com>
2023-03-28 06:08:14 +0100

    updating dice loss to be same as og iou loss

--- lossfn.py
+++ lossfn.py
@@ -49,7 +49,7 @@ def semisup_dice_loss(pred, target, pred_unlabeled, alpha, smooth=1):
     dice_loss_unlabeled = 1 - dice_unlabeled.mean()
 
     # Combining the losses
-    loss = dice_loss_labeled + alpha * dice_loss_unlabeled
+    loss = alpha * dice_loss_labeled + (1 - alpha) * dice_loss_unlabeled
 
     return loss
 
@@ -85,7 +85,7 @@ def semisup_iou_loss(pred, target, pred_unlabeled, alpha, eps=1e-6):
     iou_loss_unlabeled = 1 - iou_unlabeled.mean()
     
     # Combined loss
-    loss = iou_loss_labeled + alpha * iou_loss_unlabeled
+    loss = alpha * iou_loss_labeled + (1 - alpha) * iou_loss_unlabeled
     
     return loss
     
