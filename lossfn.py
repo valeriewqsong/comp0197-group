6904f60c0659cd296ebbb4009acefede38208ef8
meloods <melody.leom00@gmail.com>
2023-03-30 22:07:01 +0100

    lossfn.py updated

--- lossfn.py
+++ lossfn.py
@@ -1,132 +1,136 @@
 import torch
 import torch.nn.functional as F
 
-def to_one_hot(tensor, num_classes):
+def supervised_dice_loss(y_pred, y_true):
     """
-    Converts a tensor of shape (N, H, W) into a one-hot encoded tensor of shape (N, num_classes, H, W).
+    Compute the supervised Dice loss for labeled data.
 
     Args:
-        tensor (torch.Tensor): The tensor to be one-hot encoded.
-        num_classes (int): The number of classes in the one-hot encoded tensor.
+        y_pred (torch.Tensor): Predictions for labeled data, of shape (batch_size, num_classes, height, width).
+        y_true (torch.Tensor): Ground truth masks for labeled data, of shape (batch_size, num_classes, height, width).
 
     Returns:
-        torch.Tensor: The one-hot encoded tensor.
+        torch.Tensor: Supervised Dice loss.
     """
-    tensor = tensor.unsqueeze(1)
-    one_hot = torch.zeros(tensor.size(0), num_classes, tensor.size(2), tensor.size(3), device=tensor.device)
-    return one_hot.scatter_(1, tensor.long(), 1)
-
-def semisup_dice_loss(pred, target, pred_unlabeled, alpha, smooth=1):
-    """
-    Computes the semi-supervised Dice loss between labeled and unlabeled predictions and targets.
+    # Converts raw model outputs into probabilities with a range of [0, 1] to ensure that the predicted values are in the same range as the ground truth masks
+    y_pred = torch.sigmoid(y_pred)
     
-    Dice loss is a measure of the overlap between two sets, defined as:
-        Dice = (2 * intersection + smooth) / (union + smooth)
+    # Needed to prevent denominator from becoming zero
+    smooth = 1e-5   
+
+    # Compute labeled loss by comparing predicted mask with ground truth mask pixel by pixel.
+    # Sums the values for each class (num_classes) at each pixel location, over all the rows (height) and columns (width) of the image.
+    intersection = torch.sum(y_true * y_pred, dim=(1, 2, 3))
+    union = torch.sum(y_true, dim=(1, 2, 3)) + torch.sum(y_pred, dim=(1, 2, 3))
+    dice_loss = 1 - (2 * intersection + smooth) / (union + smooth)
+
+    # Average the losses across the batch
+    return dice_loss.mean()
+
+
+def supervised_iou_loss(y_pred, y_true):
+    """
+    Compute the supervised IOU loss for labeled data.
+
+    Args:
+        y_pred (torch.Tensor): Predictions for labeled data, of shape (batch_size, num_classes, height, width).
+        y_true (torch.Tensor): Ground truth masks for labeled data, of shape (batch_size, num_classes, height, width).
+
+    Returns:
+        torch.Tensor: Supervised IOU loss.
+    """
+    # Converts raw model outputs into probabilities with a range of [0, 1] to ensure that the predicted values are in the same range as the ground truth masks
+    y_pred = torch.sigmoid(y_pred)
+    
+    # Needed to prevent denominator from becoming zero
+    smooth = 1e-5   
+
+    # Compute labeled loss by comparing predicted mask with ground truth mask pixel by pixel.
+    # Sums the values for each class (num_classes) at each pixel location, over all the rows (height) and columns (width) of the image.
+    intersection = torch.sum(y_true * y_pred, dim=(1, 2, 3))
+    union = torch.sum(y_true, dim=(1, 2, 3)) + torch.sum(y_pred, dim=(1, 2, 3))
+    iou_loss = 1 - (intersection + smooth) / (union + smooth)
+
+    # Average the losses across the batch
+    return iou_loss.mean()
+
+
+def create_pseudo_labels(unlabeled_pred):
+    """
+    Generate pseudo-labels from the unlabeled predictions.
+    
+    Args:
+        unlabeled_pred (torch.Tensor): Predictions with shape (batch_size, num_classes, height, width).
         
+    Returns:
+        torch.Tensor: Pseudo-labels with shape (batch_size, num_classes, height, width), where the channel with the 
+        highest likelihood for each pixel is set to 1, and all other channels are set to 0.
+    """
+    # Get the channel with the highest likelihood for each pixel
+    max_channels = torch.argmax(unlabeled_pred, dim=1, keepdim=True)
+    
+    # Create a tensor filled with zeros, matching the shape of unlabeled_pred
+    pseudo_labels = torch.zeros_like(unlabeled_pred)
+    
+    # Set the channel with the highest likelihood for each pixel to 1
+    pseudo_labels.scatter_(1, max_channels, 1)
+    
+    return pseudo_labels
+
+
+def semi_supervised_dice_loss(y_pred, y_true, unlabeled_pred, alpha=0.5):
+    """
+    Compute the semi-supervised Dice loss for labeled and unlabeled data using pseudo-labels.
+
     Args:
-        pred (torch.Tensor): Labeled data predictions, shape (N, C, H, W)
-        target (torch.Tensor): Labeled data targets, shape (N, H, W)
-        pred_unlabeled (torch.Tensor): Unlabeled data predictions, shape (N, C, H, W)
-        alpha (float): The balancing factor between the labeled and unlabeled data contributions
-        smooth (float, optional): Smoothing factor to avoid division by zero, default=1
+        y_pred (torch.Tensor): Predictions for labeled data, of shape (batch_size, num_classes, height, width).
+        y_true (torch.Tensor): Ground truth masks for labeled data, of shape (batch_size, num_classes, height, width).
+        unlabeled_pred (torch.Tensor): Predictions for unlabeled data, of shape (batch_size, num_classes, height, width).
+        alpha (float): Weight for consistency regularization. Default is 0.5.
 
     Returns:
-        torch.Tensor: The combined Dice loss for labeled and unlabeled data
+        torch.Tensor: Semi-supervised Dice loss.
     """
-    num_classes = pred.shape[1]
-    target_one_hot = to_one_hot(target, num_classes=num_classes)
+    # Converts raw model outputs into probabilities with a range of [0, 1] to ensure that the predicted values are in the same range as the ground truth masks
+    y_pred = torch.sigmoid(y_pred)
     
-    # Labeled data
-    intersection_labeled = (pred * target_one_hot).sum(dim=(2, 3))
-    union_labeled = pred.sum(dim=(2, 3)) + target_one_hot.sum(dim=(2, 3))
-    dice_labeled = (2 * intersection_labeled + smooth) / (union_labeled + smooth)
-    dice_loss_labeled = 1 - dice_labeled.mean()
-
-    # Unlabeled data
-    true_label_unlabeled = (pred_unlabeled > 0.5).float()
-    intersection_unlabeled = (pred_unlabeled * true_label_unlabeled).sum(dim=(2, 3))
-    union_unlabeled = pred_unlabeled.sum(dim=(2, 3)) + true_label_unlabeled.sum(dim=(2, 3))
-    dice_unlabeled = (2 * intersection_unlabeled + smooth) / (union_unlabeled + smooth)
-    dice_loss_unlabeled = 1 - dice_unlabeled.mean()
+    # Compute labeled loss
+    labeled_loss = supervised_dice_loss(y_pred, y_true)
+    
+    # Generate pseudo-labels from unlabeled data
+    unlabeled_pred_pseudo = create_pseudo_labels(unlabeled_pred)
+    
+    # Compute unlabeled loss
+    unlabeled_loss = F.mse_loss(unlabeled_pred, unlabeled_pred_pseudo)
 
     # Combining the losses
-    loss = dice_loss_labeled + alpha * dice_loss_unlabeled
+    return labeled_loss + alpha * unlabeled_loss
 
-    return loss
 
-def semisup_iou_loss(pred, target, pred_unlabeled, alpha, eps=1e-6):
+def semi_supervised_iou_loss(y_pred, y_true, unlabeled_pred, alpha=0.5):
     """
-    Computes the semi-supervised IoU loss between labeled and unlabeled predictions and targets.
-    
-    The IoU loss is a measure of the overlap between two sets, defined as:
-        IoU = (intersection + eps) / (union + eps)
+    Compute the semi-supervised IOU loss for labeled and unlabeled data using pseudo-labels.
+
     Args:
-        pred (torch.Tensor): Labeled data predictions, shape (N, C, H, W)
-        target (torch.Tensor): Labeled data targets, shape (N, H, W)
-        pred_unlabeled (torch.Tensor): Unlabeled data predictions, shape (N, C, H, W)
-        alpha (float): The balancing factor between the labeled and unlabeled data contributions
-        eps (float, optional): Small constant to avoid division by zero, default=1e-6
+        y_pred (torch.Tensor): Predictions for labeled data, of shape (batch_size, num_classes, height, width).
+        y_true (torch.Tensor): Ground truth masks for labeled data, of shape (batch_size, num_classes, height, width).
+        unlabeled_pred (torch.Tensor): Predictions for unlabeled data, of shape (batch_size, num_classes, height, width).
+        alpha (float): Weight for consistency regularization. Default is 0.5.
 
     Returns:
-        torch.Tensor: The combined IoU loss for labeled and unlabeled data
+        torch.Tensor: Semi-supervised IOU loss.
     """
-    target_one_hot = to_one_hot(target, num_classes=37)
+    # Converts raw model outputs into probabilities with a range of [0, 1] to ensure that the predicted values are in the same range as the ground truth masks
+    y_pred = torch.sigmoid(y_pred)
     
-    # Labeled data
-    intersection_labeled = (pred * target_one_hot).sum(dim=(2, 3))
-    union_labeled = pred.sum(dim=(2, 3)) + target_one_hot.sum(dim=(2, 3)) - intersection_labeled
-    iou_labeled = (intersection_labeled + eps) / (union_labeled + eps)
-    iou_loss_labeled = 1 - iou_labeled.mean()
+    # Compute labeled loss
+    labeled_loss = supervised_iou_loss(y_pred, y_true)
+    
+    # Generate pseudo-labels from unlabeled data
+    unlabeled_pred_pseudo = create_pseudo_labels(unlabeled_pred)
+    
+    # Compute unlabeled loss
+    unlabeled_loss = F.mse_loss(unlabeled_pred, unlabeled_pred_pseudo)
 
-    # Unlabeled data
-    true_label_unlabeled = (pred_unlabeled > 0.5).float()
-    intersection_unlabeled = (pred_unlabeled * true_label_unlabeled).sum(dim=(2, 3))
-    union_unlabeled = pred_unlabeled.sum(dim=(2, 3)) + true_label_unlabeled.sum(dim=(2, 3)) - intersection_unlabeled
-    iou_unlabeled = (intersection_unlabeled + eps) / (union_unlabeled + eps)
-    iou_loss_unlabeled = 1 - iou_unlabeled.mean()
-    
-    # Combined loss
-    loss = alpha * iou_loss_labeled + (1 - alpha) * iou_loss_unlabeled
-    
-    return loss
-    
-def dice_loss_and_score(pred, target, smooth=1):
-    """
-    Computes the Dice loss between predictions and targets and returns the loss and score.
-    
-    Args:
-        pred (torch.Tensor): Predictions, shape (N, C, H, W)
-        target (torch.Tensor): Targets, shape (N, C, H, W)
-        smooth (float, optional): Smoothing factor to avoid division by zero, default=1
-
-    Returns:
-        Tuple[torch.Tensor, float]: The Dice loss and score
-    """
-    target_one_hot = to_one_hot(target, num_classes=37)
-    intersection = (pred * target_one_hot).sum(dim=(2, 3))
-    union = pred.sum(dim=(2, 3)) + target_one_hot.sum(dim=(2, 3))
-    dice = (2 * intersection + smooth) / (union + smooth)
-    dice_loss = 1 - dice.mean()
-    dice_score = dice.mean().item()
-    return dice_loss, dice_score
-
-def iou_loss_and_score(pred, target, eps=1e-6):
-    """
-    Computes the IoU loss between predictions and targets.
-    
-    Args:
-        pred (torch.Tensor): Predictions, shape (N, C, H, W)
-        target (torch.Tensor): Targets, shape (N, C, H, W)
-        eps (float, optional): Small constant to avoid division by zero, default=1e-6
-
-    Returns:
-        tuple(torch.Tensor, float): The IoU loss and the average IoU score
-    """
-    target_one_hot = to_one_hot(target, num_classes=37)
-    intersection = (pred * target_one_hot).sum(dim=(2, 3))
-    union = pred.sum(dim=(2, 3)) + target_one_hot.sum(dim=(2, 3)) - intersection
-    iou = (intersection + eps) / (union + eps)
-    iou_loss = 1 - iou.mean()
-    iou_score = iou.mean().item()
-    return iou_loss, iou_score
-
+    # Combining the losses
+    return labeled_loss + alpha * unlabeled_loss
--- task.py
+++ task.py
@@ -6,10 +6,10 @@ from data_loader import get_data_loader
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(device)
 
-train_labeld_loader, train_unlabeled_loader, test_loader = get_data_loader()
+train_labeled_loader, train_unlabeled_loader, test_loader = get_data_loader()
 # train model with dice loss
 dice_trained_model = train_segmentation_model(
-    train_labeld_loader,
+    train_labeled_loader,
     train_unlabeled_loader,
     test_loader,
     device,
@@ -23,7 +23,7 @@ print(f'Model trained with dice loss saved.')
 
 # train model with iou loss
 iou_trained_model = train_segmentation_model(
-    train_labeld_loader,
+    train_labeled_loader,
     train_unlabeled_loader,
     test_loader,
     device,
