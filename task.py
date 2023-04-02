2182894d613eb52306c2bc08021bbae603883e0e
meloods <melody.leom00@gmail.com>
2023-04-02 01:34:21 +0100

    upper bound, lower bound, diff ratio

--- task.py
+++ task.py
@@ -1,4 +1,4 @@
-from train import train_segmentation_model
+from train import train_labeled_and_unlabeled, train_labeled_onlt
 import torch
 from data_loader import get_data_loader
 
@@ -6,35 +6,141 @@ base_dir = "./"
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(device)
 
-# train model with iou loss. label to unlabeled data is 4:1
-train_labeled_loader, train_unlabeled_loader, test_loader = get_data_loader(base_dir, batch_size=4, ratio=0.25)
-print("Ratio of labeled to unlabeled data is 4:1")
-iou_trained_model = train_segmentation_model(
+# Upper bound performance: all data is labeled
+train_labeled_loader, train_unlabeled_loader, test_loader = get_data_loader(base_dir, batch_size=4, ratio=0.0)
+print("All the data is labeled.")
+iou_trained_model = train_labeled_and_unlabeled(
     train_labeled_loader,
     train_unlabeled_loader,
     test_loader,
     device,
-    num_epochs=100,        
+    num_epochs=2,        
     lr=1e-3, 
     use_dice = False
 )
 # save trained model
-torch.save(iou_trained_model.state_dict(), f'saved_model_iou_4to1.pt')
-print(f'Model trained with iou loss saved.')
+torch.save(iou_trained_model.state_dict(), f'saved_model_iou_1to1.pt')
+print('Model trained with iou loss and all labeled data saved.')
 
-# train model with iou loss. label to unlabeled data is 1:4
-train_labeled_loader, train_unlabeled_loader, test_loader = get_data_loader(base_dir, batch_size=4, ratio=4.0)
-print("Ratio of labeled to unlabeled data is 1:4")
-iou_trained_model = train_segmentation_model(
+# High labeled data ratio. labeled to unlabeled ratio is 1:1
+train_labeled_loader, train_unlabeled_loader, test_loader = get_data_loader(base_dir, batch_size=4, ratio=1.0)
+print("Equal split, ratio of labeled to unlabeled data is 1:1")
+iou_trained_model = train_labeled_and_unlabeled(
     train_labeled_loader,
     train_unlabeled_loader,
     test_loader,
     device,
-    num_epochs=100,        
+    num_epochs=2,        
     lr=1e-3, 
     use_dice = False
 )
 # save trained model
-torch.save(iou_trained_model.state_dict(), f'saved_model_iou_1to4.pt')
-print(f'Model trained with iou loss saved.')
+torch.save(iou_trained_model.state_dict(), f'saved_model_iou_1to1.pt')
+print('Model trained with iou loss and 1:1 ratio saved.')
+
+# Lower bound performance: only labeled data is used
+print("Equal split, ratio of labeled to unlabeled data is 1:1, but only labeled data is used here.")
+iou_trained_model = train_labeled_only(
+    train_labeled_loader,
+    test_loader,
+    device,
+    num_epochs=2,        
+    lr=1e-3, 
+    use_dice = False
+)
+# save trained model
+torch.save(iou_trained_model.state_dict(), f'saved_model_iou_1to1_labeled_only.pt')
+print('Model trained with iou loss and 1:1 ratio but labeled only saved.')
+
+
+# Moderate labeled data ratio. labeled to unlabeled ratio is 1:3
+train_labeled_loader, train_unlabeled_loader, test_loader = get_data_loader(base_dir, batch_size=4, ratio=3.0)
+print("Ratio of labeled to unlabeled data is 1:3")
+iou_trained_model = train_labeled_and_unlabeled(
+    train_labeled_loader,
+    train_unlabeled_loader,
+    test_loader,
+    device,
+    num_epochs=2,        
+    lr=1e-3, 
+    use_dice = False
+)
+# save trained model
+torch.save(iou_trained_model.state_dict(), f'saved_model_iou_1to3.pt')
+print('Model trained with iou loss and 1:3 ratio saved.')
+
+# Lower bound performance: only labeled data is used
+print("Ratio of labeled to unlabeled data is 1:3, but only labeled data is used here.")
+iou_trained_model = train_labeled_only(
+    train_labeled_loader,
+    test_loader,
+    device,
+    num_epochs=2,        
+    lr=1e-3, 
+    use_dice = False
+)
+# save trained model
+torch.save(iou_trained_model.state_dict(), f'saved_model_iou_1to3_labeled_only.pt')
+print('Model trained with iou loss and 1:3 ratio but labeled only saved.')
+
+
+# Moderate labeled data ratio. labeled to unlabeled ratio is 1:5
+train_labeled_loader, train_unlabeled_loader, test_loader = get_data_loader(base_dir, batch_size=4, ratio=5.0)
+print("Ratio of labeled to unlabeled data is 1:5")
+iou_trained_model = train_labeled_and_unlabeled(
+    train_labeled_loader,
+    train_unlabeled_loader,
+    test_loader,
+    device,
+    num_epochs=2,        
+    lr=1e-3, 
+    use_dice = False
+)
+# save trained model
+torch.save(iou_trained_model.state_dict(), f'saved_model_iou_1to5.pt')
+print('Model trained with iou loss and 1:5 ratio saved.')
+
+# Lower bound performance: only labeled data is used
+print("Ratio of labeled to unlabeled data is 1:5, but only labeled data is used here.")
+iou_trained_model = train_labeled_only(
+    train_labeled_loader,
+    test_loader,
+    device,
+    num_epochs=2,        
+    lr=1e-3, 
+    use_dice = False
+)
+# save trained model
+torch.save(iou_trained_model.state_dict(), f'saved_model_iou_1to5_labeled_only.pt')
+print('Model trained with iou loss and 1:5 ratio but labeled only saved.')
+
+# High labeled data ratio. labeled to unlabeled ratio is 1:10
+train_labeled_loader, train_unlabeled_loader, test_loader = get_data_loader(base_dir, batch_size=4, ratio=10.0)
+print("Ratio of labeled to unlabeled data is 1:10")
+iou_trained_model = train_labeled_and_unlabeled(
+    train_labeled_loader,
+    train_unlabeled_loader,
+    test_loader,
+    device,
+    num_epochs=2,        
+    lr=1e-3, 
+    use_dice = False
+)
+# save trained model
+torch.save(iou_trained_model.state_dict(), f'saved_model_iou_1to10.pt')
+print('Model trained with iou loss and 1:10 ratio saved.')
+
+# Lower bound performance: only labeled data is used
+print("Ratio of labeled to unlabeled data is 1:10, but only labeled data is used here.")
+iou_trained_model = train_labeled_only(
+    train_labeled_loader,
+    test_loader,
+    device,
+    num_epochs=2,        
+    lr=1e-3, 
+    use_dice = False
+)
+# save trained model
+torch.save(iou_trained_model.state_dict(), f'saved_model_iou_1to10_labeled_only.pt')
+print('Model trained with iou loss and 1:10 ratio but labeled only saved.')
 
--- train.py
+++ train.py
@@ -4,7 +4,7 @@ import torch.optim as optim
 from linknet import link_net
 from lossfn_1ch import supervised_dice_loss, supervised_iou_loss, semi_supervised_dice_loss, semi_supervised_iou_loss, create_pseudo_labels
 
-def train_segmentation_model(train_loader_with_label, train_loader_without_label, test_loader, device, num_epochs=50, lr=1e-4, use_dice=True):
+def train_labeled_and_unlabeled(train_loader_with_label, train_loader_without_label, test_loader, device, num_epochs=50, lr=1e-4, use_dice=True):
     """
     Train a semi-supervised segmentation model with labeled and unlabeled data.
 
@@ -112,6 +112,94 @@ def train_segmentation_model(train_loader_with_label, train_loader_without_label
         else:
             print('Epoch {}, Test Loss: {:.6f} IoU Score: {:.6f}'.format(epoch+1, test_loss/len(test_loader), total_iou_score/len(test_loader)))
 
+    print("Training completed.")
+
+    return model
+
+def train_labeled_only(train_loader_with_label, test_loader, device, num_epochs=50, lr=1e-4, use_dice=True):
+    """
+    Train a supervised segmentation model with labeled data only.
+
+    Args:
+        train_loader_with_label (DataLoader): Labeled training data loader.
+        test_loader (DataLoader): Test/validation data loader.
+        device (str): Device to run the training on, e.g., "cpu" or "cuda".
+        num_epochs (int, optional): Number of training epochs. Default is 50.
+        lr (float, optional): Learning rate for the optimizer. Default is 1e-4.
+        use_dice (bool, optional): If True, dice loss used. If IOU loss used. Default is True.
+
+    Returns:
+        nn.Module: The trained segmentation model.
+    """
+    # Initialize the neural network
+    model = link_net(classes=1).to(device)
+
+    # Define loss function and optimizer
+    optimizer = optim.Adam(model.parameters(), lr=lr)
+
+    if use_dice:
+        print("Training with dice loss function...")
+    else:
+        print("Training with iou loss function..")
+
+    for epoch in range(num_epochs):
+        running_loss = 0.0
+
+        # Train on labeled data only
+        for i, (images_with_label, labels) in enumerate(train_loader_with_label):
+            images_with_label, labels = images_with_label.to(device), labels.to(device)
+
+            # zero the parameter gradients
+            optimizer.zero_grad()
+
+            # forward + backward + optimize
+            pred_with_label = model(images_with_label)
+
+            # determine the loss function used
+            if use_dice:
+                loss = supervised_dice_loss(pred_with_label, labels)
+            else:
+                loss = supervised_iou_loss(pred_with_label, labels)
+
+            loss.backward()
+            optimizer.step()
+
+            # print statistics every 5 iterations
+            running_loss += loss.item()
+            if i % 5 == 4:
+                print(f"Epoch {epoch+1}, iteration {i+1}: loss = {running_loss / 5:.6f}")
+                running_loss = 0.0
+
+        # Evaluate the model on the test set
+        model.eval()
+
+        test_loss = 0
+        total_iou_score = 0
+        total_dice_score = 0
+        accuracy = 0
+
+        with torch.no_grad():
+            for data, target in test_loader:
+                data, target = data.to(device), target.to(device)
+                output = model(data)
+
+                if use_dice:
+                    dice_loss = supervised_dice_loss(output, target)
+                    dice_score = 1 - dice_loss.item()
+                    test_loss += dice_loss.item()
+                    total_dice_score += dice_score
+
+                else:
+                    iou_loss = supervised_iou_loss(output, target)
+                    iou_score = 1 - iou_loss.item()
+                    test_loss += iou_loss.item()
+                    total_iou_score += iou_score
+
+        if use_dice:
+            print('Epoch {}, Test Loss: {:.6f} Dice Score: {:.6f}'.format(epoch+1, test_loss/len(test_loader), total_dice_score/len(test_loader)))
+        else:
+            print('Epoch {}, Test Loss: {:.6f} IoU Score: {:.6f}'.format(epoch+1, test_loss/len(test_loader), total_iou_score/len(test_loader)))
+            
     print("Training completed.")
 
     return model
