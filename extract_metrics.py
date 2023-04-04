adb4d3c5148297972dc5d30c60583e551fbf2f22
meloods <melody.leom00@gmail.com>
2023-04-04 21:09:05 +0100

    ratios 1to5 and 1to10 training completed

---
+++ extract_metrics.py
@@ -0,0 +1,41 @@
+import re
+import csv
+
+def txt_to_csv(input_txt, output_csv):
+    # Read .txt file
+    with open(input_txt, 'r') as f:
+        lines = f.readlines()
+
+    # Extract data
+    data = []
+
+    for line in lines:
+        if "After Epoch" in line:
+            epoch = int(re.search(r'Epoch (\d+)', line).group(1))
+            val_loss = re.search(r'Validation loss = (nan|[\d.]+)', line).group(1)
+            iou_score = re.search(r'IoU Score = (nan|[\d.]+)', line).group(1)
+            dice_score = re.search(r'Dice Score = (nan|[\d.]+)', line).group(1)
+            precision = re.search(r'Precision = (nan|[\d.]+)', line).group(1)
+            recall = re.search(r'Recall = (nan|[\d.]+)', line).group(1)
+            specificity = re.search(r'Specificity = (nan|[\d.]+)', line).group(1)
+
+            val_loss = float(val_loss) if val_loss != 'nan' else float('nan')
+            iou_score = float(iou_score) if iou_score != 'nan' else float('nan')
+            dice_score = float(dice_score) if dice_score != 'nan' else float('nan')
+            precision = float(precision) if precision != 'nan' else float('nan')
+            recall = float(recall) if recall != 'nan' else float('nan')
+            specificity = float(specificity) if specificity != 'nan' else float('nan')
+
+            data.append([epoch, val_loss, iou_score, dice_score, precision, recall, specificity])
+
+    # Write .csv file
+    with open(output_csv, 'w', newline='') as csvfile:
+        csv_writer = csv.writer(csvfile)
+        csv_writer.writerow(['Epoch', 'Validation Loss', 'IoU Score', 'Dice Score', 'Precision', 'Recall', 'Specificity'])
+        csv_writer.writerows(data)
+
+# This part assumes that you have already unzipped the zip files.
+txt_to_csv('output_txt/output_1to5_unlabeled.txt', 'csv_metrics/metrics_1to5_unlabeled.csv')
+txt_to_csv('output_txt/output_1to5_labeled.txt', 'csv_metrics/metrics_1to5_labeled.csv')
+txt_to_csv('output_txt/output_1to10_unlabeled.txt', 'csv_metrics/metrics_1to10_unlabeled.csv')
+txt_to_csv('output_txt/output_1to10_labeled.txt', 'csv_metrics/metrics_1to10_labeled.csv')
--- task.py
+++ task.py
@@ -109,6 +109,35 @@ torch.save(iou_trained_model.state_dict(), f'saved_model_iou_1to3_labeled_only.p
 print('Model trained with iou loss and 1:3 ratio but labeled only saved.\n')
 
 
+# High unlabeled data ratio. labeled to unlabeled ratio is 1:10
+train_labeled_loader, train_unlabeled_loader, val_labeled_loader, test_labeled_loader = get_data_loader(base_dir, batch_size=32, UtoL_ratio=10.0)
+print("Ratio of labeled to unlabeled data is 1:10")
+iou_trained_model = train_labeled_and_unlabeled(
+    train_labeled_loader,
+    train_unlabeled_loader,
+    val_labeled_loader,
+    device,
+    num_epochs=100,        
+    lr=1e-5
+)
+# save trained model
+torch.save(iou_trained_model.state_dict(), f'saved_model_iou_1to10.pt')
+print('Model trained with iou loss and 1:10 ratio saved.\n')
+
+# Lower bound performance: only labeled data is used
+print("Ratio of labeled to unlabeled data is 1:10, but only labeled data is used here.")
+iou_trained_model = train_labeled_only(
+    train_labeled_loader,
+    val_labeled_loader,
+    device,
+    num_epochs=100,        
+    lr=1e-5
+)
+# save trained model
+torch.save(iou_trained_model.state_dict(), f'saved_model_iou_1to10_labeled_only.pt')
+print('Model trained with iou loss and 1:10 ratio but labeled only saved.\n')
+
+
 # Moderate labeled data ratio. labeled to unlabeled ratio is 1:5
 train_labeled_loader, train_unlabeled_loader, val_labeled_loader, test_labeled_loader = get_data_loader(base_dir, batch_size=32, UtoL_ratio=5.0)
 print("Ratio of labeled to unlabeled data is 1:5")
@@ -138,31 +167,5 @@ torch.save(iou_trained_model.state_dict(), f'saved_model_iou_1to5_labeled_only.p
 print('Model trained with iou loss and 1:5 ratio but labeled only saved.\n')
 
 
-# High unlabeled data ratio. labeled to unlabeled ratio is 1:10
-train_labeled_loader, train_unlabeled_loader, val_labeled_loader, test_labeled_loader = get_data_loader(base_dir, batch_size=32, UtoL_ratio=10.0)
-print("Ratio of labeled to unlabeled data is 1:10")
-iou_trained_model = train_labeled_and_unlabeled(
-    train_labeled_loader,
-    train_unlabeled_loader,
-    val_labeled_loader,
-    device,
-    num_epochs=100,        
-    lr=1e-5
-)
-# save trained model
-torch.save(iou_trained_model.state_dict(), f'saved_model_iou_1to10.pt')
-print('Model trained with iou loss and 1:10 ratio saved.\n')
 
-# Lower bound performance: only labeled data is used
-print("Ratio of labeled to unlabeled data is 1:10, but only labeled data is used here.")
-iou_trained_model = train_labeled_only(
-    train_labeled_loader,
-    val_labeled_loader,
-    device,
-    num_epochs=100,        
-    lr=1e-5
-)
-# save trained model
-torch.save(iou_trained_model.state_dict(), f'saved_model_iou_1to10_labeled_only.pt')
-print('Model trained with iou loss and 1:10 ratio but labeled only saved.\n')
 
