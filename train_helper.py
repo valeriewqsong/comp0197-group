ec88dcdb8841e0d78695c781624406ab7bf0c7da
meloods <melody.leom00@gmail.com>
2023-04-04 01:26:44 +0100

    semi sup bce loss fn created

--- train_helper.py
+++ train_helper.py
@@ -1,5 +1,39 @@
 import torch
+import torch.nn as nn
 import torch.nn.functional as F
+
+def semi_supervised_bce_loss(y_pred, y_true, unlabeled_pred, alpha=0.5):
+    """
+    Compute the semi-supervised BCE loss for labeled and unlabeled data using pseudo-labels.
+
+    Args:
+        y_pred (torch.Tensor): Predictions for labeled data, of shape (batch_size, num_classes, height, width).
+        y_true (torch.Tensor): Ground truth masks for labeled data, of shape (batch_size, num_classes, height, width).
+        unlabeled_pred (torch.Tensor): Predictions for unlabeled data, of shape (batch_size, num_classes, height, width).
+        alpha (float): Weight for consistency regularization. Default is 0.5.
+
+    Returns:
+        torch.Tensor: Semi-supervised IOU loss.
+    """
+    # Binary Cross Entropy loss function.
+    # Sigmoid function will be applied to convert raw model outputs into probabilities with a range of [0, 1].
+    criterion = nn.BCEWithLogitsLoss()
+    
+    # Compute labeled loss
+    labeled_loss = criterion(y_pred, y_true)
+    
+    # Generate pseudo-labels from unlabeled data
+    # Apply sigmoid to convert raw model outputs into probabilities with a range of [0, 1]
+    # Convert the probabilities into binary values (0 or 1) using a threshold of 0.5
+    unlabeled_pred_pseudo = (torch.sigmoid(unlabeled_pred) > 0.5).float()
+    
+    # Compute unlabeled loss
+    unlabeled_loss = criterion(unlabeled_pred, unlabeled_pred_pseudo)
+
+    # Combining the losses
+    return labeled_loss + alpha * unlabeled_loss, labeled_loss, unlabeled_loss
+
+
 
 def dice_score(y_pred, y_true, smooth=1e-5):
     """
